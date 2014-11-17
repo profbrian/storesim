86a757eecf21bc60d15421a61e21c38a639b14a8
Brian Smith <blsmithers@gmail.com>
2014-11-16 19:07:37 -0500

    Add line template

---
+++ src/edu/marist/cmpt220l/storesim/Line.java
@@ -0,0 +1,25 @@
+package edu.marist.cmpt220l.storesim;
+
+/**
+ * Created by brian on 11/16/14.
+ */
+public class Line {
+
+    public void addCustomer(Customer customer) {
+        //TODO: implement
+    }
+
+    public Customer getNextCustomer() {
+        return null;
+        //TODO: implemeent
+    }
+
+    public boolean hasCustomers() {
+        return false;
+        //TODO: implement
+    }
+
+    public void printLine() {
+        //TODO: implement
+    }
+}
