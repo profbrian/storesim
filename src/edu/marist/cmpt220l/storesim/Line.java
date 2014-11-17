c148e33d0219ec84078c8e12df4392693335eed2
profbrian <blsmithers@gmail.com>
2014-11-16 19:23:40 -0500

    Add comments to describe functions

--- src/edu/marist/cmpt220l/storesim/Line.java
+++ src/edu/marist/cmpt220l/storesim/Line.java
@@ -5,20 +5,42 @@ package edu.marist.cmpt220l.storesim;
  */
 public class Line {
 
+    /**
+     * Add a new customer to the line
+     * 
+     * @param customer the new customer that is added to the back of the line
+     */
     public void addCustomer(Customer customer) {
         //TODO: implement
     }
 
+    /**
+     * Retrieve the next customer (the customer at the front of the line)
+     * 
+     * @returns the next customer
+     */
     public Customer getNextCustomer() {
         return null;
         //TODO: implemeent
     }
 
+    /**
+     * Retrieve whether there are any customers waiting in line.
+     * 
+     * @returns true if there are customers waiting in line, false otherwise.
+     */
     public boolean hasCustomers() {
         return false;
         //TODO: implement
     }
 
+    /**
+     * Print the current line.  For each customer, print out how long that customer has been waiting.
+     * For example, if there were 4 customers on line it might look like this:
+     *       "Line: (4)--(4)--(3)--(2)"
+     * Meaning the first two customers have a wait time of 4, the 3rd customer a wait time of 3, the 4th
+     * customer a wait time of 2.
+     */
     public void printLine() {
         //TODO: implement
     }
